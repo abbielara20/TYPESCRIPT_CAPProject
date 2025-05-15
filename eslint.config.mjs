b89d9e69ffb5e9cb74f3c235a9adc8eab059b0f7
Abbie Jane Lara <abbie.jane.z.lara@accenture.com>
2025-05-15 01:46:24 +0000

    AJZL: init commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,19 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
+import cdsPlugin from "@sap/eslint-plugin-cds";
 
 
 export default defineConfig([
+  {
+    ignores: ["**/dist/*", "gen/**/*", "@cds-models/**/*", "app/**/*"],
+  },
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
+  cdsPlugin.configs.recommended,
   {
 		rules: {
 			"no-unused-vars": "warn"
